1c9969d92484d85330eea2eb39d1ec2bf5cdd3be
Arjun <kiriti.k999@gmail.com>
2025-06-27 01:49:39 +0530

    update profile api calls

--- apps/web/apis/v1/profile/profile.ts
+++ apps/web/apis/v1/profile/profile.ts
@@ -1,33 +1,32 @@
-import { courseApiClient } from '@whatsnxt/core-util';
+import { commonApiClient } from '@whatsnxt/core-util';
 
 export const ProfileAPI = {
-  getProfile: async function (token) {
-    console.log(' token:', token)
-    const response = await courseApiClient.get('/common/profile', token);
+  getProfile: async function () {
+    const response = await commonApiClient.get('/profile');
     return response;
   },
   getEditProfile: async function () {
-    const response = await courseApiClient.patch('/common/profile/edit-profile', {});
+    const response = await commonApiClient.patch('/profile/edit-profile', {});
     return response;
   },
   resetPasswordRequest: async function (payload) {
-    const response = await courseApiClient.post('/common/profile/reset-password-request', payload);
+    const response = await commonApiClient.post('/profile/reset-password-request', payload);
     return response;
   },
   editPassword: async function (payload) {
-    const response = await courseApiClient.patch('/common/profile/edit-password', payload);
+    const response = await commonApiClient.patch('/profile/edit-password', payload);
     return response;
   },
   editProfile: async function (payload) {
-    const response = await courseApiClient.patch('/common/profile/edit-profile', payload);
+    const response = await commonApiClient.patch('/profile/edit-profile', payload);
     return response;
   },
   editProfileInfo: async function (payload) {
-    const response = await courseApiClient.patch('/common/profile/edit-profile-info', payload);
+    const response = await commonApiClient.patch('/profile/edit-profile-info', payload);
     return response;
   },
   setPassword: async function (payload) {
-    const response = await courseApiClient.post(`/common/profile/reset-password/${payload.id}/${payload.token}`, { password: payload.password });
+    const response = await commonApiClient.post(`/profile/reset-password/${payload.id}/${payload.token}`, { password: payload.password });
     return response;
   },
 };
