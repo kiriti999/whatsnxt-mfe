7e66bf45631f0b33b7b6ba58475b1e968e35b15b
Arjun <kiriti.k999@gmail.com>
2025-07-08 00:52:20 +0530

    truncate linkedIn tecxt

--- packages/core-ui/src/ShareOptions/LinkedInShare.tsx
+++ packages/core-ui/src/ShareOptions/LinkedInShare.tsx
@@ -37,7 +37,7 @@ const LinkedInShare: React.FC<LinkedInShareProps> = ({ url, title, thumbnailUrn,
         checkTokenStatus();
     }, [email]);
 
-    const truncateLinkedInText = (text: string, maxLength = 1600) => {
+    const truncateLinkedInText = (text: string, maxLength = 2000) => {
         if (!text) return '';
 
         // If text is within limit, return as is
