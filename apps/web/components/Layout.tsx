4e3965866a1656bd4ccf0880eff7f05186b5c8f6
Arjun <kiriti.k999@gmail.com>
2025-07-12 04:09:27 +0530

    rm import

--- apps/web/components/Layout.tsx
+++ apps/web/components/Layout.tsx
@@ -1,6 +1,3 @@
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
-
 import React from 'react';
 import Navbar from './Navbar';
 import { Footer } from '@whatsnxt/core-ui/src/Footer';
