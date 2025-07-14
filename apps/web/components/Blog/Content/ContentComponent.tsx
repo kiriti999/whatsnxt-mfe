eb1258c88d58f48fdca3d4837d9457e52ae2e4ea
Arjun <kiriti.k999@gmail.com>
2025-07-14 15:54:14 +0530

    update google indexing

--- apps/web/components/Blog/Content/ContentComponent.tsx
+++ apps/web/components/Blog/Content/ContentComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../../store/store';
+import { AppDispatch } from '../../../store/store';
 import {
   getPosts,
   getTutorials,
