4a0a9edb1d0477f2b515984f4802ce45d3c29b69
Arjun Komaragiri <kiriti.k999@gmail.com>
2025-12-10 17:56:41 +0530

    fix theme in contact us page

--- apps/web/app/_component/contact-us/ContactIcons.tsx
+++ apps/web/app/_component/contact-us/ContactIcons.tsx
@@ -26,11 +26,11 @@ export function ContactIconsList() {
 }
 
 const infoList: ContactIconProps[] = [
-  { description: <Text fz={18}>info@whatsnxt.in</Text>, icon: <IconMail size={24} /> },
-  { description: <Text fz={18}>+91 6300711966</Text>, icon: <IconPhoneCall size={24} /> },
-  { description: <Text fz={18}>Hyderabad, India</Text>, icon: <IconMapPin size={24} /> },
-  { description: <Text fz={18}>Sunday & Saturday: 10:30 - 15:00</Text>, icon: <IconClockHour3 size={24} /> },
-  { description: <Text fz={18}>Monday - Friday: 09:00 - 20:00</Text>, icon: <IconClockHour3 size={24} /> },
+  { description: <Text fz={18} c="white">info@whatsnxt.in</Text>, icon: <IconMail size={24} /> },
+  { description: <Text fz={18} c="white">+91 6300711966</Text>, icon: <IconPhoneCall size={24} /> },
+  { description: <Text fz={18} c="white">Hyderabad, India</Text>, icon: <IconMapPin size={24} /> },
+  { description: <Text fz={18} c="white">Sunday & Saturday: 10:30 - 15:00</Text>, icon: <IconClockHour3 size={24} /> },
+  { description: <Text fz={18} c="white">Monday - Friday: 09:00 - 20:00</Text>, icon: <IconClockHour3 size={24} /> },
 ];
 
 interface ContactIconProps
